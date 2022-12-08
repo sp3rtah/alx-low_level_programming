7e07f9d7cc949ece2c0d4f428ccee3f3b1fe1fe6
sp3rtah <ngaira14nelson@gmail.com>
2022-12-08 04:55:52 -0800

    new file:   100-intel new file:   101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fwrite(quote, sizeof(quote[0]), sizeof(quote) / sizeof(quote[0]), stderr);
+	return (0);
+}
