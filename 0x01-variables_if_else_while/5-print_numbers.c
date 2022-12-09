023af285e2de576b88b65ff7a825bbba61c0fded
sp3rtah <ngaira14nelson@gmail.com>
2022-12-09 10:30:09 -0800

    new file:   5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
