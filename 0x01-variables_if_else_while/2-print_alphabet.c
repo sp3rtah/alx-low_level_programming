8eefd5974d5a46af20f4fe9f2966290be3d50770
sp3rtah <ngaira14nelson@gmail.com>
2022-12-09 10:22:35 -0800

    new file: print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
