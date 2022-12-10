503fdb6c5be49040b39eee26f56240047dd3cb63
sp3rtah <ngaira14nelson@gmail.com>
2022-12-10 00:17:56 -0800

    Fixing betty linter issues

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,68 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 #include <stdbool.h>
+#include "./typedefs.h"
 
 /**
- * main - Entry point
- * Return: 0 on success
+ * show_permutation - Print results of the permutaion
+ * @printed: array of structs
+ * @total: Total items in struct above
+ * Return: void
  */
-int main(void)
+void show_permutation(printer *printed, int total)
 {
-	struct printed
-	{
-		int a;
-		int b;
-	} printed[100];
-
-	int a, b;
-	int i, j, k;
-	bool skip;
-	int total, count;
+	int i;
 	int loop_count;
-	const int nums[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-	total = 0;
-	count = sizeof(nums) / sizeof(int);
-
-	for (i = 0; i < count; i++)
-		for (j = 0; j < count; j++)
-		{
-			a = nums[i];
-			b = nums[j];
-
-			skip = false;
-
-			if (a == b)
-				continue;
-
-			for (k = 0; k < total; k++)
-			{
-				if (a == printed[k].a)
-					if (b == printed[k].b)
-						skip = true;
-
-				if (a == printed[k].b)
-					if (b == printed[k].a)
-						skip = true;
-			}
-
-			if (skip)
-				continue;
-
-			printed[total].a = a;
-			printed[total].b = b;
-			++total;
-		}
 	loop_count = 0;
-
 	for (i = 0; i < total; i++)
 	{
 		putchar(printed[i].a);
 		putchar(printed[i].b);
 
-		if (loop_count!= total - 1)
+		if (loop_count != total - 1)
 		{
 			putchar(',');
 			putchar(' ');
@@ -70,5 +29,63 @@ int main(void)
 		loop_count++;
 	}
 	putchar('\n');
+}
+
+/**
+ * permutate - Creates a collection of the required parmutations
+ * @printed: array of structures to be printed
+ * @total: number of items in array of structues
+ * Return: void
+ */
+void permutate(printer *printed, int *total)
+{
+	bool skip;
+	int count;
+	int a, b, i, j, k;
+	int nums[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	*total = 0;
+	count = sizeof(nums) / sizeof(int);
+
+	for (i = 0; i < count; i++)
+		for (j = 0; j < count; j++)
+		{
+			skip = false;
+			a = nums[i];
+			b = nums[j];
+			if (a == b)
+				continue;
+			for (k = 0; k < *total; k++)
+			{
+				if (a == printed[k].a)
+					if (b == printed[k].b)
+						skip = true;
+				if (a == printed[k].b)
+					if (b == printed[k].a)
+						skip = true;
+			}
+			if (skip)
+				continue;
+			printed[*total].a = a;
+			printed[*total].b = b;
+			++(*total);
+		}
+}
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int total;
+	size_t size;
+	printer *printed;
+
+	total = 0;
+	size = sizeof(printer) * 100;
+	printed = malloc(size);
+
+	permutate(printed, &total);
+	show_permutation(printed, total);
 	return (0);
 }
