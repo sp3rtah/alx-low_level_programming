ec41fec19501e83ac57cfe1e8eb5ef68c550d77f
sp3rtah <ngaira14nelson@protonmail.com>
2022-12-14 10:42:51 +0300

    9-times_table.c working

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
 #include "main.h"
+
+#define ROW_COUNT 10
+#define COL_COUNT 10
 /**
  * times_table - Prints 9 * 9 table with leading zeros
  * Return: void
  */
 void times_table(void)
 {
-	int i;
-	int nums[10];
+	int row, col;
+	int value;
+	int tens, ones;
 
-	for (i = 0; i < 10; i++)
-		nums[i] = i;
+	for (row = 0; row < ROW_COUNT; row++)
+		for (col = 0; col < COL_COUNT; col++)
+		{
+			tens = 0;
+			value = row * col;
+			ones = value % 10;
 
-	for (i = 0; i < 10; i++)
-	{
-		_putchar('0' + nums[i]);
-		_putchar(',');
-		_putchar(' ');
-	}
+			if (value > ones)
+				tens = value / 10;
+
+			if (tens == 0)
+			{
+				if (col != 0)
+					_putchar(' ');
+				_putchar('0' + ones);
+			}
+			else
+			{
+				_putchar('0' + tens);
+				_putchar('0' + ones);
+			}
+
+			if (col == 9)
+				_putchar('\n');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
 }
