2b92e59769cb5689e68692ef62226b4aaebe7a1f
sp3rtah <ngaira14nelson@protonmail.com>
2022-12-14 10:59:18 +0300

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,30 @@
  */
 void print_to_98(int n)
 {
-	if (n == 98)
-		printf("%d\n", n);
+	int i;
 
 	if (n < 98)
-		for (; n <= 98; n++)
+	{
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", n);
+			printf("%d", i);
+
+			if (i != 98)
+				printf(", ");
+			else
+				printf("\n");
 		}
+	}
 	else
-		for (; n >= 98; n--)
+	{
+		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", n);
+			printf("%d", i);
+
+			if (i != 98)
+				printf(", ");
+			else
+				printf("\n");
 		}
-	printf("\n");
+	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,6 @@ int print_sign(int);
 int _isalpha(int);
 int _islower(int);
 void print_alphabet_x10(void);
-void print_to_98(int);
 int add(int a, int b);
 void print_alphabet(void);
+void print_to_98(int);
