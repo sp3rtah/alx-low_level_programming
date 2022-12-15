a99ed66b7f9a126bdd3ab07efd9089d1c90d9e10
sp3rtah <ngaira14nelson@protonmail.com>
2022-12-15 14:20:05 +0300

    modified:   0x03-debugging/3-print_remaining_days.c new file:   0x04-more_functions_nested_loops/0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - Checks for uppercase character
+ * @c: character
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return ((c >= 'A' && c <= 'Z') ? 1 : 0);
+}
