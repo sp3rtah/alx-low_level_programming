cbc63b95dabe9ec96b669692da00fc6d4b6c07a8
sp3rtah <ngaira14nelson@protonmail.com>
2022-12-15 14:57:36 +0300

    new file:   6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - prints 10 times the numbers, from 0 to 14
+ * @n: int
+ */
+void print_line(int n)
+{
+	for (; n > 0; n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
