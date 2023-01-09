37d74dbbf014dee0ed757677f475c4e3fdfbc2af
sp3rtah <ngaira14nelson@protonmail.com>
2023-01-09 18:11:26 +0300

    0x09 static libs

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+/**
+ * _isdigit - Checks if a character is a digit
+ * @c: character
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return ((c >= '0' && c <= '9') ? 1 : 0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _abs - Returns the absolute of a number
+ * @n: number
+ * Return: int
+ */
+int _abs(int n)
+{
+	return ((n < 0) ? (-1 * n) : n);
+}
